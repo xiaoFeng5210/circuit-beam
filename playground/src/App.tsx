4ddb4b211a78620e7e805a8b122c07786c6c9992
Eadweard <804875728@qq.com>
2024-02-24 18:09:51 +0800

    基础搭建

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Lightning from './components/Lightning'
 
 function App() {
 
   return (
     <>
       <div className='g_box w-[700px] h-[621px] bg-slate-400 relative'>
-
+        <div className='absolute'><Lightning /></div>
       </div>
     </>
   )
