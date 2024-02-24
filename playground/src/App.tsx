730145ef7637ead7a8dfe9c9ec4451fbeff7b425
Eadweard <804875728@qq.com>
2024-02-24 17:26:14 +0800

    init

---
+++ playground/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='g_box w-[700px] h-[621px] bg-slate-400 relative'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
